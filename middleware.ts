4ae5b6dba181265f39ef12e2342b8a7c016b4147
Ruslan Batsulia <ruslan.batsulia@gmail.com>
2025-10-25 08:44:42 +0300

    befor migration middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { routing } from "@/i18n/routing";
-import createProxy from "next-intl/middleware";
+import createMiddleware from "next-intl/middleware";
  
-export default createProxy(routing);
+export default createMiddleware(routing);
  
 export const config = {
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)"
