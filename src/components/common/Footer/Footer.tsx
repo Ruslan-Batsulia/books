5eb3e79aedc2ad30eac0d811f711eb92fafe2f0e
Ruslan Batsulia <ruslan.batsulia@gmail.com>
2025-10-26 00:30:28 +0300

    footer and header

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,10 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { ChangeLanguage, ChangeTheme } from "..";
+
+
+import logo from "@/public/images/logo.svg";
+
 import "./Footer.scss";
 
 export default function Footer() {
+  const translate = useTranslations("Footer");
+  
   return (
-    <footer className={"footer"}>
-      <div className={"container"}>
-        {"Footer"}
+    <footer className="footer">
+      <div className="container">
+        <Link href={"/"} className={"footer__logo"}>
+          <Image src={logo} alt={"Logo"} className={"footer__logo-img"} />
+          <span className={"footer__logo-title"}>{"Books Catalog"}</span>
+        </Link>
+
+        <div className={"footer__settings"}>
+          <div className={"footer__language"}>
+            <span className={"footer__language-title"}>{translate("language")}</span>
+            <ChangeLanguage />
+          </div>
+          
+          <div className={"footer__theme"}>
+            <span className={"footer__theme-title"}>{translate("theme")}</span>
+            <ChangeTheme />
+          </div>
+        </div>
       </div>
     </footer>
   );
